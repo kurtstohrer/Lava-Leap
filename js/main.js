f418c3b9c2d3bd0fb5d9c86a80b4c7522a61bfca
unknown <acm3750@OH1380-25.main.ad.rit.edu>
2014-11-20 11:50:14 -0500

    fixed platform spawning

--- js/main.js
+++ js/main.js
@@ -456,6 +456,12 @@ app.main = {
 				{
 					var pwidth = 32 * Math.floor(Math.random() * 9 + 2);
 					var px = Math.random() * 800 + platforms[highestindex].x - 400;
+					var randType = Math.random();
+					var typeIndex = 0;
+					if(randType < 0.10)
+					{
+						typeIndex = 2;
+					}
 					if(px + pwidth > this.WIDTH)
 					{
 						var diff =  px + pwidth - this.WIDTH;
@@ -465,7 +471,7 @@ app.main = {
 					{
 						px = -px;
 					}
-					platforms.push(new app.Platform(pwidth, px, this.platformImage, this.platformTypes[0]));
+					platforms.push(new app.Platform(pwidth, px, this.platformImage, this.platformTypes[typeIndex]));
 				}
 				platforms = platforms.filter(function(platform)
 				{
